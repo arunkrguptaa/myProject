249e51882e308adcb36db68e403de8bb6dd18b4b
arunkrguptaa <arunkrguptaa@gmail.com>
2017-11-26 15:44:31 +0530

    new change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ stop: 'Mother fuckker' });
+    res.send({ stop: 'Mother fuckksdkcsker' });
 });
 
 const PORT = process.env.PORT || 5000;

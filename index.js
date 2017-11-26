2fced661de61bae2ab06fe799a8697f8e9eafbf9
arunkrguptaa <arunkrguptaa@gmail.com>
2017-11-26 15:43:00 +0530

    change file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ bte: 'fuck' });
+    res.send({ stop: 'Mother fuckker' });
 });
 
 const PORT = process.env.PORT || 5000;

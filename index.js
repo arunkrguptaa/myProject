d2be922279337943c6ceb8b7dc051e87b9f6fe55
arunkrguptaa <arunkrguptaa@gmail.com>
2017-11-27 22:13:33 +0530

    comit
    nkin

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const express = require('express');
 //const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-//87946249453-2hrup7q2p0bbb9ftajqg2vj4jbr2l1ug.apps.googleusercontent.com 
-//NAWCSYIyX6VBqGq1Y9F5W_n3 
+
 
 //passport.use(new GoogleStrategy());
-app.get('/', ( req, res ) => {
-    res.send({hey: 'bitch'});
-});
 
 const PORT = process.env.PORT || 5000;
 

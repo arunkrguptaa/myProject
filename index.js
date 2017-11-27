8fe651cc7edeebac792652ad4f89f2b1ff1e20f8
arunkrguptaa <arunkrguptaa@gmail.com>
2017-11-27 08:42:02 +0530

    new changes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const app = express();
+//const passport = require('passport');
+//const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
-    res.send({ stop: 'Mother fuckksdkcsker' });
+const app = express();
+//87946249453-2hrup7q2p0bbb9ftajqg2vj4jbr2l1ug.apps.googleusercontent.com 
+//NAWCSYIyX6VBqGq1Y9F5W_n3 
+
+//passport.use(new GoogleStrategy());
+app.get('/', ( req, res ) => {
+    res.send({hey: 'bitch'});
 });
 
 const PORT = process.env.PORT || 5000;

35ddbfc5e519a052b2cf30083df48a39d66716cf
arunkrguptaa <arunkrguptaa@gmail.com>
2017-12-03 15:50:33 +0530

    files
    fioes

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
-//const passport = require('passport');
-//const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./config/key');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/Users');
+require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
- 
-//passport.use(new GoogleStrategy());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000;
 
